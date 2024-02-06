cb8392a879e5def1f3a12ab63a0b6c9194e96b04
hhennessyy <juandavidytm1@gmail.com>
2024-02-05 19:17:24 -0500

    Agrego sintaxis de las intrucciones de control: @if - @for - @switch

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -7,18 +7,19 @@ type Grade = 'A' | 'B' | 'F';
   standalone: true,
   imports: [],
   templateUrl: './control-flow.component.html',
-  styles: ``
+  styles: `` 
 })
 export default class ControlFlowComponent {
 
   public showContent = signal(false);
 
   public toggleContent() {
-    this.showContent.update( value => !value )
-
-
+    this.showContent.update( value  => !value )
   }
 
-  public grade = signal<Grade>('F');
+  public grade = signal<Grade>('A');
+
+  public frameworks = signal([ 'Angular', 'Svelte', 'React', 'Vue', 'Qwik' ]);
+  public frameworks2 = signal([]);
 
 }
