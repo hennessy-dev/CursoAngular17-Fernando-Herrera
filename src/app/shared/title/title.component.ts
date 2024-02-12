ac07c87519607bd0a8d861032d2365cc3bbbb1e6
hhennessyy <juandavidytm1@gmail.com>
2024-02-11 22:51:41 -0500

    feat: :sparkles: Agrega la propiedad transform: booleanAttribute
    Agrega la propiedad transform: booleanAttribute al input para configurar la propiedad withShadow como booleana, evitando asi tener que especificarlo explicitamente, ej: withShadow="true", ahora solo con nombrar la propiedad es suficiente para inferir que es true

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,15 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
   template: `
-    <h1 class="text-3xl mb-5 font-extrabold text-center">{{ title }}</h1>
+    <h1 class="text-3xl mb-5 font-extrabold text-center">{{ title }} - {{ withShadow }}</h1>
   `,
   styles: ``
 })
 
 export default class TitleComponent {
   @Input({ required: true }) title: string = '';
+  @Input({ transform: booleanAttribute  }) withShadow: boolean = false;
 }
