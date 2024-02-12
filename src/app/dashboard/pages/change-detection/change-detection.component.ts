d25633446a04febeb6c3358db482a33acd762a25
hhennessyy <juandavidytm1@gmail.com>
2024-02-12 15:06:16 -0500

    feat: :sparkles: Modifica la propiedad ChangeDetectionStrategy y la configura en "On Push"
    Modifica el change detection con el objetivo de lograr un mejor rendimiento logrando que angular reduzca la cantidad de veces que verifica los componentes, haciendo que el componente se actualice cuando exista trigger o evento

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 import TitleComponent from '../../../shared/title/title.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [TitleComponent],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  imports: [TitleComponent, CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+
+    <app-title [title]="currentFramework()" ></app-title>
+
+
+    <pre> {{ frameworkAsSignal() | json }} </pre>
+    <pre> {{ frameworkAsProperty | json }} </pre>
+
+  `,
 })
 export default class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change Detection - ${this.frameworkAsSignal().name}`
+  );
 
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016
+  })
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016
+  };
+
+  constructor() {
+
+    setTimeout(() => {
+      console.log("Hecho");
+
+      // this.frameworkAsProperty.name = ".NET framework ";
+      this.frameworkAsSignal.update(value => {
+        value.name = "React"; 
+
+
+        return {...value}
+      })
+
+
+
+    }, 100);
+
+  }
 }
